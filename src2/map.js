ca9284672b689ad2be60de4917e16695d7ae87d5
jacintoluf <jacinto.luf@gmail.com>
2020-12-14 19:47:58 +0000

    map.js update

--- src2/map.js
+++ src2/map.js
@@ -3,34 +3,30 @@ var init_url = 'https://api.mapbox.com/v4/mapbox.terrain-rgb/';
 var end_url = '.pngraw?access_token='+key;
 var zoom = 0;
 var tile = Math.pow(2,2*zoom);
-var x_pos = y_pos = Math.floor(Math.pow(2,zoom)-1);
+var x_max = y_max = Math.floor(Math.pow(2,zoom)-1);
+var x_pos = y_pos = 0;
+
 
 function calc_tile(){
     tile = Math.pow(2,2*zoom);
 }
 function calc_pos(){
-    x_pos = y_pos = Math.floor(Math.pow(2,zoom)-1/2);
+    x_pos = x_max/2;
+    y_pos = y_max/2;
 }
 
-
 function map(){
-    if(sceneModels[0].sx<1 && zoom>0){
-        zoom--;
-        calc_tile(); calc_pos();
-        console.log("zoom: "+String(zoom)+" tile:"+String(tile));
-        m_url = init_url+String(zoom)+'/'+String(x_pos)+'/'+String(y_pos)+end_url;
-        
-        //sceneModels[0] = new terrain(m_url, meshDepth);
-        //sceneModels[0].sx = sceneModels[0].sy = sceneModels[0].sz = 1;
-        //console.log(m_url);
-    }
-    else if(sceneModels[0].sx>1.5 && zoom<11){
-        zoom++;
-        calc_tile(); calc_pos();
-        console.log("zoom: "+String(zoom)+" tile:"+String(tile));
-        m_url = init_url+String(zoom)+'/'+String(x_pos)+'/'+String(y_pos)+end_url;
-        //sceneModels[0] = new terrain(m_url, meshDepth);
-        //sceneModels[0].sx = sceneModels[0].sy = sceneModels[0].sz = 1;
-        //console.log(m_url);
-    }
+    calc_tile(); calc_pos();
+    console.log("zoom: "+String(zoom)+" tile:"+String(tile));
+    m_url = init_url+String(zoom)+'/'+String(x_pos)+'/'+String(y_pos)+end_url;
+    sceneModels[0] = new terrain(m_url, meshDepth);
+    sceneModels[0].sx = sceneModels[0].sy = sceneModels[0].sz = 0.5;
+}
+function walk_map(x=0, y=0){
+    x_pos += x;
+    y_pos += y;
+    console.log("zoom: "+String(zoom)+" tile:"+String(tile));
+    m_url = init_url+String(zoom)+'/'+String(x_pos)+'/'+String(y_pos)+end_url;
+    sceneModels[0] = new terrain(m_url, meshDepth);
+    sceneModels[0].sx = sceneModels[0].sy = sceneModels[0].sz = 0.5;
 }
